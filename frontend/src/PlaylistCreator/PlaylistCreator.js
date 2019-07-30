fde4132de7ee1514273dcf818865e79ce431250a
Charles Todd <cpt8@duke.edu>
2019-07-30 00:56:49 -0400

    more fields added

--- frontend/src/PlaylistCreator/PlaylistCreator.js
+++ frontend/src/PlaylistCreator/PlaylistCreator.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import { Button, Form, FormGroup, Label, Input, FormText, Containter, Row, Col } from 'reactstrap';
 import NavbarComponent from '../NavbarComponent/NavbarComponent.js';
 import { Nav } from 'react-bootstrap';
 import './PlaylistCreator.css';
@@ -11,14 +11,15 @@ export default class PlaylistCreator extends Component {
     super();
     this.state = {
       params: 
-      {      
-        playlist_size:-1,
-        artists: ''
+      {     
+        // playlist_size:-1,
+        // artists: ''
         // genres:''
       },
       genre_options:['rap',
                      'pop',
-                     'pop rap']
+                     'pop rap'],
+      rangedParamsList: ['Release Date', 'Liked Date','Tempo','Acousticness','Danceability','Energeticness','Instrumentalness','Valence']
     }
     this.handleChange = this.handleChange.bind(this)
     this.submit = this.submit.bind(this)
@@ -26,13 +27,13 @@ export default class PlaylistCreator extends Component {
 
   handleChange(event)
   {
-    let param_name = event.target.name;
+    let param_id = event.target.id;
     let value = event.target.value;
     //this.setState((name, value) => {console.log(name,value); return {name: value}})
     let params = this.state.params;
-    params[param_name] = value;
+    params[param_id] = value;
     this.setState({params: params});
-    console.log('onChange method called!',event.target.name,event.target.value);
+    console.log('onChange method called!',event.target.id,event.target.value);
     console.log('state:',this.state)
   }
 
@@ -84,7 +85,32 @@ export default class PlaylistCreator extends Component {
             <Label for="exampleText">Artists</Label>
             <Input type="textarea" name="artists" id="artists" onChange={this.handleChange} />
           </FormGroup>
-          
+
+          <FormGroup>
+            <Label for="exampleText">Genres</Label>
+            <Input type="textarea" name="genres" id="genres" onChange={this.handleChange} />
+          </FormGroup>          
+          {
+           this.state.rangedParamsList.map(key => 
+            <div>
+              {key} <text id={key.toLowerCase().replace(' ','_') + "_format"} className="format">(YYYY-MM-DD)</text>
+              <Row>
+                <Col className="ranged-input">
+                  <FormGroup>
+                    <Label for="exampleText">Minimum </Label>
+                    <Input type="textarea" id={(key + "_lower").toLowerCase().replace(' ','_')} onChange={this.handleChange} />
+                  </FormGroup>
+                </Col>
+                <Col className="ranged-input">
+                  <FormGroup>
+                    <Label for="exampleText">Maximum </Label>
+                    <Input type="textarea" id={(key + "_upper").toLowerCase().replace(' ','_')} onChange={this.handleChange} />
+                  </FormGroup>
+                </Col>
+              </Row>
+           </div>)
+          } 
+
           {/* <FormGroup>
             <Label for="exampleSelectMulti">Genres</Label>
             <Input type="textarea" name="genres" id="genres" onChange={this.handleChange} />
